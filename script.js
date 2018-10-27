add1a7899b099f4e0a309db71bc945a7ed2957fc
Emmanuel <emmanuel1441@hotmail.com>
2018-10-26 21:31:22 -0500

    initial commit

--- script.js
+++ script.js
@@ -7,6 +7,41 @@ function displayLocation(position) {
 
 	var pLocation = document.getElementById("location");
 	pLocation.innerHTML +=  latitude + "," + longitude + "<br>";
+
+	var pInfo = document.getElementById("info");
+	var date = new Date(position.timestamp);
+	pInfo.innerHTML = "Location timestamp:" + date + "<br>";
+	pInfo.innerHTML += "Accuracy of location:" + position.coords.accuracy + "meters<br>";
+
+	if(position.coords.altitude){
+		pInfo.innerHTML += "Altitude: " + position.coords.altitude;
+	}
+	else {
+		console.log("Altitude: ", position.coords.altitude);
+	}
+
+    if(position.coords.altitudeAccuracy){
+		pInfo.innerHTML += "with accuracy: " + position.coords.altitudeAccuracy + "in meters";
+	}
+	else {
+		console.log("Altitude accuracy: ", position.coords.altitudeAccuracy);
+	}
+	pInfo.innerHTML+="<br>";
+
+	if(position.coords.heading){
+		pInfo.innerHTML += "Heading: " + position.coords.heading+ "<br>";
+	}
+	else {
+		console.log("Heading is : ", position.coords.heading);
+	}
+
+    if(position.coords.speed){
+		pInfo.innerHTML += "Speed: " + position.coords.speed + "<br>";
+	}
+	else {
+		console.log("Speed is: ", position.coords.speed);
+	}
+
 }
 
 function displayError(error){ 
